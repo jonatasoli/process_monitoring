f184f77c2cfa44a321a42105eb39affce72628cd
Jonatas Oliveira <contato@jonatasoliveira.me>
2018-02-27 10:45:01 -0300

    Customize Django Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import StatusProcess
 
-# Register your models here.
+
+class StatusProcessModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_profile', 'id_process', 'status_process')
+    search_fields = ('user_profile', 'id_process', 'status_process')
+    list_filter = ('user_profile',)
+
+
+admin.site.register(StatusProcess, StatusProcessModelAdmin)
