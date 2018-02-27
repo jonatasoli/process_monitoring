15803214a7b1af9c473bc88feeb1895e9d63901c
Jonatas Oliveira <contato@jonatasoliveira.me>
2018-02-27 08:18:30 -0300

    Create basic tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from core.models import StatusProcess
 
-# Create your tests here.
+
+class UrlValid(TestCase):
+    def setUp(self):
+        self.api = self.client.get('/api/')
+        self.profile = self.client.get('/api/profile/')
+        self.login = self.client.get('/api/login/')
+        self.status = self.client.get('/api/status-process/')
+
+    def test_get(self):
+        """Get /api/* must retrun status code """
+        self.assertEqual(200, self.profile.status_code)
+        self.assertEqual(405, self.login.status_code)
+        self.assertEqual(200, self.status.status_code)
+        self.assertEqual(200, self.api.status_code)
+
+
+class StatusProcessModelTest(TestCase):
+
+    def setUp(self):
+        self.obj = StatusProcess(
+                userprofile='1',
+                id_process='12345678901',
+                status_process='Em análise'
+                )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(StatusProcess.objects.exists())
+
+    def test_str(self):
+        self.assertEqual('Em análise', str(self.obj))
